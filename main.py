3fb2a95f6c9ef681053c872603a9dec8b393d150
13Mai13 <maialen.berrondo@auth0.com>
2022-06-19 18:41:10 +0200

    post request

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello World v2'}
 
+
 @app.get("/posts")
 async def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+async def create_posts():
+    return {"message": "Successfully created post"}
